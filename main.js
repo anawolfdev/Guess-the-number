9824b1be0d0cfc1d31b60c0187bc36f1dbc8b40e
Ana Amaro <amarovazquezana@gmail.com>
2019-07-29 17:05:44 +0200

    Completed main.js

--- main.js
+++ main.js
@@ -4,10 +4,56 @@ const button = document.querySelector('.button');
 const input = document.querySelector('.input');
 const counter = document.querySelector('.counter');
 const result = document.querySelector('.result');
-const myRandomNumber = random(100); //num random hasta 100
+const RandomNumber = random(100); //num random hasta 100
+let clicks = 0;
+counter.innerHTML = clicks;
 
 // funcion para calcular números aleatorios max=100 //
+
 function random(max) {
     return Math.floor(Math.random() * max);
 };
+
+// Función que compare el número del usuario del input con el número aleatorio
+
+function randomCompared() {
+    const userNumber = parseInt(input.value);
+    if (RandomNumber === userNumber) {
+        printResult(result, '¡HAS GANADO, CAMPEONA!');
+    } else if (userNumber > 100) {
+        printResult(result, 'No puede ser mayor de 100');
+    } else if (userNumber < 0) {
+        printResult(result, 'No puede ser menor que 0');
+    } else if (RandomNumber < userNumber) {
+        printResult(result, 'demasiado alto');
+    } else if (RandomNumber > userNumber) {
+        printResult(result, 'demasiado bajo');
+        addCounter();
+    } else {
+        printResult(result, 'Por favor introduce un número válido');
+    }
+}
+
+// Función para imprimir el resultado //
+
+function printResult(constName, text) {
+    constName.innerHTML = text;
+}
+
+// Función para añadir +1 en el contador
+
+function addCounter() {
+    clicks += 1;
+    counter.innerHTML = clicks;
+}
+
+// Evento para llamar a las funciones randomCompared y addCounter //
+function handleButtonClick() {
+    randomCompared();
+    addCounter();
+}
+
+// EventListener del botón //
+
+button.addEventListener('click', handleButtonClick);
 
