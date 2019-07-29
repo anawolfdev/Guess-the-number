cf03c7e1926f56dfb0007984ef2e5a83895db54e
Ana Amaro <amarovazquezana@gmail.com>
2019-07-29 16:00:30 +0200

    Add styles in main.css and include function random number for main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const button = document.querySelector('.button');
+const input = document.querySelector('.input');
+const counter = document.querySelector('.counter');
+const result = document.querySelector('.result');
+const myRandomNumber = random(100); //num random hasta 100
+
+// funcion para calcular números aleatorios max=100 //
+function random(max) {
+    return Math.floor(Math.random() * max);
+};
+
